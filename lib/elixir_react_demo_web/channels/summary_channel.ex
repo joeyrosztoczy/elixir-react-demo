30b7400dfea536fd084230d0260faa72ac10cef0
joeyrosztoczy <joeyr@leaf.ag>
2019-03-11 18:20:20 -0600

    add summary channel

---
+++ lib/elixir_react_demo_web/channels/summary_channel.ex
@@ -0,0 +1,25 @@
+defmodule ElixirReactDemoWeb.SummaryChannel do
+  @moduledoc """
+  Channel to provide data to react via websockets.
+  """
+  use ElixirReactDemoWeb, :channel
+
+  alias ElixirReactDemo.Station
+
+  def join("summary:stations", _payload, socket) do
+    # Would demo auth but in a hurry
+    {:ok, socket}
+  end
+
+  # Client API
+  # def broadcast_stations(msg) do
+  #  ElixirReactDemoWeb.Endpoint.broadcast!("summary:stations", "new_stations", msg)
+  # end
+
+  # Server API
+  def handle_in("request_stations", _body, socket) do
+    stations = Station.get_stations()
+
+    {:reply, {:ok, %{stations: stations}}, socket}
+  end
+end
