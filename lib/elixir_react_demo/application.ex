53e88f5b9d6991d446137e8b4d4159d5feda23b9
joeyrosztoczy <joeyr@leaf.ag>
2019-03-11 21:30:28 -0600

    remove repo for easier startup

--- lib/elixir_react_demo/application.ex
+++ lib/elixir_react_demo/application.ex
@@ -9,7 +9,7 @@ defmodule ElixirReactDemo.Application do
     # List all child processes to be supervised
     children = [
       # Start the Ecto repository
-      ElixirReactDemo.Repo,
+      # ElixirReactDemo.Repo,
       # Start the endpoint when the application starts
       ElixirReactDemoWeb.Endpoint,
       # Starts a worker by calling: ElixirReactDemo.start_link(arg)
